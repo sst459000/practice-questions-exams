0302e1064637188eb40e4c4e50b4a2e716e88081
Sourav459000 <100828121+Sourav459000@users.noreply.github.com>
2026-08-01 01:22:00 +0530

    updated configs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/practice-questions-exams/",
+  base: "/",
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.js",
